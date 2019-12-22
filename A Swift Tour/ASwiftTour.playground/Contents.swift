f0033ac2300fa00494f531747d6a855d6e512683
Mohamed Anwar <manwar@mobilenoc.mobi>
2019-12-23 01:15:56 +0200

    updates

--- A Swift Tour/ASwiftTour.playground/Contents.swift
+++ A Swift Tour/ASwiftTour.playground/Contents.swift
@@ -294,6 +294,7 @@ func send(job: Int, toPrinter printerName: String) throws -> String {
 }
 
 // MARK :- Genircs
+// MARK :-  Modify the anyCommonElements(_:_:) function to make a function that returns an array of the elements that any two sequences have in common.
 
 func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> [T.Element]
     where T.Element: Equatable, T.Element == U.Element {
